58e17cdbf17fcbf3d642b026e30f55bf5e6e99ab
Mélanie D <melanie.diligent@gmail.com>
2018-11-27 15:15:19 +0100

    Override

--- src/main/java/com/pizzeria/console/IPizzaDao.java
+++ src/main/java/com/pizzeria/console/IPizzaDao.java
@@ -1,17 +1,13 @@
 package com.pizzeria.console;
 
 import java.util.List;
-
-import com.pizzeria.exception.DeletePizzaException;
-import com.pizzeria.exception.SavePizzaException;
-import com.pizzeria.exception.UpDatePizzaException;
-
 public interface IPizzaDao {
+	
 
 	 List<Pizza> findAllPizzas();
-	 void saveNewPizza(Pizza pizza) throws SavePizzaException;
-	 void updatePizza(String codePizza, Pizza pizza) throws UpDatePizzaException;
-	 void deletePizza(String codePizza) throws DeletePizzaException;
+	 void saveNewPizza(Pizza pizza);
+	 void updatePizza(String codePizza, Pizza pizza);
+	 void deletePizza(String codePizza);
 	 Pizza findPizzaByCode(String codePizza);
 	 boolean isPizzaExists(String codePizza);
 	
--- src/main/java/com/pizzeria/console/Pizza.java
+++ src/main/java/com/pizzeria/console/Pizza.java
@@ -1,65 +1,43 @@
 package com.pizzeria.console;
 
-import com.pizzeria.model.CategoriePizzaEnum;
-import com.pizzeria.exception.StockageException;
-
 public class Pizza {
-
-	//attributs de la classe
+	
 	private int id;
 	private String code;
 	private String designation;
 	private Double prix;
-	private CategoriePizzaEnum categoriePizza ;
 	
-	private final int LONGUEUR_CODE = 3;
-	private final int PRIX_MAX = 15;
-	private final int PRIX_MIN = 4;
 	
 	public static int compteur = 0;
-
+	
 	//Constructeur
-	/**
-	 * @param code code pizza
-	 * @param designation nom pizza
-	 * @param prix prix de la pizza à l'unité
-	 */
-	public Pizza(String code, String designation, Double prix) {
+	 	/**
+	 	 * @param code code pizza
+		 * @param designation nom pizza
+		 * @param prix prix de la pizza à l'unité
+		 */
+
+public Pizza(String code, String designation, Double prix) {
 		
 		this.id = ++compteur;
 		this.code = code;
-		this.designation = designation;
-		this.prix = prix;
+ 	}
+ 	
+ 	public String getCode() {
+ 		return code;
+ 	}
+ 	public void setCode(String code) {
+ 		this.code = code;
+ 	}
+ 
+ 	
+ 	public String getDesignation() {
+ 		return designation;
+ 	}
+ 	public void setDesignation(String designation) {
+ 		this.designation = designation;
 	}
 	
-	public Pizza( String code, String designation, Double prix, CategoriePizzaEnum categoriePizza) {
-		super();
-		this.id = ++compteur;
-		this.code = code;
-		this.designation = designation;
-		this.prix = prix;
-		this.categoriePizza = categoriePizza;
-	}
-
-	//Les getters et setters
-	public int getId() {
-		return id;
-	}
-
-	public String getCode() {
-		return code;
-	}
-	public void setCode(String code) {
-		this.code = code;
-	}
-
-	public String getDesignation() {
-		return designation;
-	}
-	public void setDesignation(String designation) {
-		this.designation = designation;
-	}
-
 	public Double getPrix() {
 		return prix;
 	}
@@ -67,27 +45,11 @@ public class Pizza {
 		this.prix = prix;
 	}
 	
-	public CategoriePizzaEnum getCategoriePizza() {
-		return categoriePizza;
-	}
-	public void setCategoriePizza(CategoriePizzaEnum categoriePizza) {
-		this.categoriePizza = categoriePizza;
-	}
-	
-	public void dataControl() throws StockageException {
-		
-		if(code.length() != LONGUEUR_CODE || code.trim().length() == 0) {
-			throw new StockageException("Pizza : Code pizza est vide ou ne contient pas 3 caractères");
-		}
-		
-		else if (prix < PRIX_MIN || prix > PRIX_MAX) {
-			
-			throw new StockageException("Pizza : Le prix est trop bas ou trop élevé");					
-		}	
-	}
 	
 	@Override
 	public String toString() {
-		return code+"->"+designation+"("+prix+") Catégorie : "+categoriePizza+"\n" ; 
-	}
+		//return "id: " + id + "\ncode: " + code + "\ndesignation: " + designation + "\nprix : " + prix + " €";
+
+		return code+"->"+designation+"("+prix+")\n" ; 
+	}	
 }
